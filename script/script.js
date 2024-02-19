b3b4e3fc9fd331882ed89b9f9d8efd62078be7c5
BasirAhmedMia <basirahmed1693@gmail.com>
2024-02-19 23:52:51 +0600

    complete

--- script/script.js
+++ script/script.js
@@ -8,12 +8,41 @@ document.getElementById('bnrBtn').addEventListener('click', function(){
 // select the button 
 let buttons = document.getElementsByClassName('green-btn');
 let count = 0;
+let seatLeft = 8;
+
 
 for(const button of buttons){
     button.addEventListener('click', function(){
         let select = button.style.backgroundColor = 'limegreen';
-        count = count + 1;
-        document.getElementById('total-seat').innerText = count;
+        count = count + 1
+        setInnerText('total-seat', count);        
     })
+}
+
+// seat left
+
+for(const button of buttons){
+    button.addEventListener('click', function(){
+        seatLeft = seatLeft - 1;
+        setInnerText('seat-left', seatLeft);
+        
+    })
+}
+// ticket add
+const ticketClass = 'Economy';
+
+// seat price
+let seatPrice = 550;
+function seatPriceCount(){
+    const totalSeatPrice = seatPrice * count;
+    const priceElement = document.getElementById('total-price').innerText = totalSeatPrice;
+    
+}
+
+
+// innerText
+function setInnerText(id, value){
+    document.getElementById(id).innerText = value;
+   
 }
 
