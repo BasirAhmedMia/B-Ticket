f1a8dc634bda3573e165ed8fb929a76d186e2462
BasirAhmedMia <basirahmed1693@gmail.com>
2024-02-19 06:06:37 +0600

    first

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// function seatGreen(){
+//     document.body.style.backgroundColor = 'green';
+
+// }
+document.getElementById('A1').addEventListener('click', function (){
+    document.body.style.backgroundColor = 'green';
+})
+
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+    // console.log('makeGreen')
+}
